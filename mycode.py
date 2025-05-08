c8dc120bed055d73b2433856f6f2f8d92b3dd420
md shahil ansari <ansarirahish455@gmail.com>
2025-05-08 18:41:58 +0530

    remove the 2nd gf

--- mycode.py
+++ mycode.py
@@ -10,16 +10,16 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 df = pd.DataFrame(data)
 
 # # Adding new row to df for V2
-new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+new_row_loc = {'Name': 'GF1', 'Age': 21, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
-# # Adding new row to df for V3
-new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
-df.loc[len(df.index)] = new_row_loc2
+# # # Adding new row to df for V3
+# new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+# df.loc[len(df.index)] = new_row_loc2
 
-# # Adding new row to df for V3
-new_row_loc3 = {'Name': 'GF3', 'Age': 30, 'City': 'City2'}
-df.loc[len(df.index)] = new_row_loc3
+# # # Adding new row to df for V3
+# new_row_loc3 = {'Name': 'GF3', 'Age': 30, 'City': 'City2'}
+# df.loc[len(df.index)] = new_row_loc3
 
 
 # Ensure the "data" directory exists at the root level
